188e0844d09005a3739d531f12f2d264c9eed3c9
Jack Gallacher <jackleslie.gallacher@gmail.com>
2016-05-08 20:06:08 +0100

    Started Chapter 5, made an ASP calculator.
    - Used C# with asp elements to make a basic calculator.

---
+++ ASP.net/Planet Wrox (Built upon each chapter)/PlanetWrox/PlanetWrox/D. Demos/CalculatorDemo.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CalculatorDemo.aspx.cs" Inherits="PlanetWrox.D._Demos.CalculatorDemo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Table ID="Table1" runat="server" GridLines="Both" CellPadding="5">
+            <asp:Tablerow>
+                <asp:TableCell ColumnSpan="3" HorizontalAlign="Center">Column1</asp:TableCell>
+            </asp:Tablerow>
+            <asp:Tablerow>
+                <asp:TableCell>Column1</asp:TableCell>
+                <asp:TableCell>Column2</asp:TableCell>
+                <asp:TableCell>Column3</asp:TableCell>
+            </asp:Tablerow>
+            <asp:Tablerow>
+                <asp:TableCell>Column1</asp:TableCell>
+                <asp:TableCell>Column2</asp:TableCell>
+                <asp:TableCell>Column3</asp:TableCell>
+            </asp:Tablerow>
+        </asp:Table>    
+        <asp:Label ID="ResultLabel" runat="server"/>
+        <asp:TextBox ID="ValueBox1" runat="server"/>
+        <asp:DropDownList ID="OperatorList" runat="server">
+            <asp:ListItem>+</asp:ListItem>
+            <asp:ListItem>-</asp:ListItem>
+            <asp:ListItem>*</asp:ListItem>
+            <asp:ListItem>/</asp:ListItem>
+        </asp:DropDownList> 
+        <asp:TextBox ID="ValueBox2" runat="server"/>
+        <asp:Button ID="CalculateButton" runat="server" Text="Calculate" OnClick="CalculateButton_Click"/>
+    </div>
+    </form>
+</body>
+</html>
---
+++ ASP.net/Planet Wrox (Built upon each chapter)/PlanetWrox/PlanetWrox/D. Demos/CalculatorDemo.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PlanetWrox.D._Demos
+{
+    public partial class CalculatorDemo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CalculateButton_Click(object sender, EventArgs e)
+        {
+            double FirstValue = 0;
+            double SecondValue = 0;
+
+            FirstValue = Convert.ToDouble(ValueBox1.Text);//Converts the text value of the "ValueBox" to a double so it can be used for arithmetic in the switch statement below.
+            SecondValue = Convert.ToDouble(ValueBox2.Text);
+
+            switch(OperatorList.SelectedValue)//Switch statement that triggers based on the "SelectedValue" of the asp:DropdownList with the ID of "OperatorList".
+            {
+                case "+":
+                    ResultLabel.Text = Convert.ToString(FirstValue + SecondValue);//Sets the "ResultLabel" to the arithmetic of FirstValue and SecondValue converted to string.
+                    break;
+                case "-":
+                    ResultLabel.Text = Convert.ToString(FirstValue - SecondValue);
+                    break;
+                case "*":
+                    ResultLabel.Text = Convert.ToString(FirstValue * SecondValue);
+                    break;
+                case "/":
+                    ResultLabel.Text = Convert.ToString(FirstValue / SecondValue);
+                    break;
+            }
+        }
+    }
+}
