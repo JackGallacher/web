b5f84c504e9526645146974e63d6890870f2dc4f
Jack Gallacher <jackleslie.gallacher@gmail.com>
2016-05-09 07:26:26 +0100

    Finished Chapter 5, used App_Code
    - Used "App_Code" to store a calculator class and access it in the web
    form. "App_Code" is a folder which can store all pieces of code that
    might be used in multiple forms. If you only need the code for one form,
    you should not put it in the "App_Code" folder.

---
+++ ASP.net/Planet Wrox (Built upon each chapter)/PlanetWrox/PlanetWrox/App_Code/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlanetWrox.App_Code
+{
+    public class Calculator
+    {
+        public double Add(double A, double B)
+        {
+            return A + B;
+        }
+        public double Subtract(double A, double B)
+        {
+            return A - B;
+        }
+        public double Multiply(double A, double B)
+        {
+            return A * B;
+        }
+        public double Divide(double A, double B)
+        {
+            return A / B;
+        }
+    }
+}
--- ASP.net/Planet Wrox (Built upon each chapter)/PlanetWrox/PlanetWrox/D. Demos/CalculatorDemo.aspx.cs
+++ ASP.net/Planet Wrox (Built upon each chapter)/PlanetWrox/PlanetWrox/D. Demos/CalculatorDemo.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using PlanetWrox.App_Code;//You need to include a reference to the "App_Code" folder to use the classes created in it.
+
 
 namespace PlanetWrox.D._Demos
 {
@@ -18,25 +20,34 @@ namespace PlanetWrox.D._Demos
         {
             double FirstValue = 0;
             double SecondValue = 0;
+            double Result = 0;
 
             FirstValue = Convert.ToDouble(ValueBox1.Text);//Converts the text value of the "ValueBox" to a double so it can be used for arithmetic in the switch statement below.
             SecondValue = Convert.ToDouble(ValueBox2.Text);
 
+            Calculator MyCalculator = new Calculator();
+            
+
             switch(OperatorList.SelectedValue)//Switch statement that triggers based on the "SelectedValue" of the asp:DropdownList with the ID of "OperatorList".
             {
                 case "+":
-                    ResultLabel.Text = Convert.ToString(FirstValue + SecondValue);//Sets the "ResultLabel" to the arithmetic of FirstValue and SecondValue converted to string.
+                    //ResultLabel.Text = Convert.ToString(FirstValue + SecondValue);//Sets the "ResultLabel" to the arithmetic of FirstValue and SecondValue converted to string.
+                    Result = MyCalculator.Add(FirstValue, SecondValue);//Calls the function from the "Calculator" class which has been put into the "App_Code" folder.
                     break;
                 case "-":
-                    ResultLabel.Text = Convert.ToString(FirstValue - SecondValue);
+                    //ResultLabel.Text = Convert.ToString(FirstValue - SecondValue);
+                    Result = MyCalculator.Subtract(FirstValue, SecondValue);
                     break;
                 case "*":
-                    ResultLabel.Text = Convert.ToString(FirstValue * SecondValue);
+                    //ResultLabel.Text = Convert.ToString(FirstValue * SecondValue);
+                    Result = MyCalculator.Multiply(FirstValue, SecondValue);
                     break;
                 case "/":
-                    ResultLabel.Text = Convert.ToString(FirstValue / SecondValue);
+                    //ResultLabel.Text = Convert.ToString(FirstValue / SecondValue);
+                    Result = MyCalculator.Divide(FirstValue, SecondValue);
                     break;
             }
+            ResultLabel.Text = Convert.ToString(Result);
         }
     }
 }
