65ec4adce97146a478b237925de65b5fc298ab90
Jack Gallacher <jackleslie.gallacher@gmail.com>
2016-05-22 18:57:16 +0100

    Added menu.
    - Used <asp:Menu> to create a menu and then used its "DataSourceID"
    property to link it to a <asp:SiteMapDataSource> which references the
    sitemap I created.

--- ASP.net/Planet Wrox (Built upon each chapter)/PlanetWrox/PlanetWrox/A. MasterPages/Frontend.Master.designer.cs
+++ ASP.net/Planet Wrox (Built upon each chapter)/PlanetWrox/PlanetWrox/A. MasterPages/Frontend.Master.designer.cs
@@ -31,6 +31,15 @@ namespace PlanetWrox.MasterPages {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// SiteMapDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SiteMapDataSource SiteMapDataSource1;
+        
+        /// <summary>
         /// cpMainContent control.
         /// </summary>
         /// <remarks>
