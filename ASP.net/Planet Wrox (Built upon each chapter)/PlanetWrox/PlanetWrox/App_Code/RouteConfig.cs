88f0b2d7f32c7774bbfd55e039cefef42639b0f2
Jack Gallacher <jackleslie.gallacher@gmail.com>
2016-06-05 20:39:23 +0100

    Added page routing
    - Added the FriendlyURL package to the project to allow extension-less
    URLs.
    - Added code that enables FriendlyURL (RouteConfig).
    - Added a Global Application Class and set the routes on
    Application_Start.

---
+++ ASP.net/Planet Wrox (Built upon each chapter)/PlanetWrox/PlanetWrox/App_Code/RouteConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using Microsoft.AspNet.FriendlyUrls;
+
+
+//This code lets us use friendly URLs when browsing the website. for example: "planetwrox.com/default.aspx" is changed to "planetwrox.com/default".
+namespace PlanetWrox.App_Code
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection Routes)
+        {
+            var Settings = new FriendlyUrlSettings();
+            Settings.AutoRedirectMode = RedirectMode.Permanent;//Makes the redirect of the URLs perminant.
+            Routes.EnableFriendlyUrls(Settings);//Enables the FriendlyURL setting for the site.
+        }
+    }
+}
