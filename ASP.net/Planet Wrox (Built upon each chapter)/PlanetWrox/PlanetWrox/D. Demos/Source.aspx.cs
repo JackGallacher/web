a1a6e7f9aa2ea4f839fa9dd5087ece0383cb56cd
Jack Gallacher <jackleslie.gallacher@gmail.com>
2016-06-08 16:37:03 +0100

    Finished Chapter 7

--- ASP.net/Planet Wrox (Built upon each chapter)/PlanetWrox/PlanetWrox/D. Demos/Source.aspx.cs
+++ ASP.net/Planet Wrox (Built upon each chapter)/PlanetWrox/PlanetWrox/D. Demos/Source.aspx.cs
@@ -13,7 +13,7 @@ namespace PlanetWrox
         protected void Page_Load(object sender, EventArgs e)
         {
             //Response.Redirect("Target?Test=SomeValue");//This redirects the user to the page "Target.aspx" when this page loads and sends the value "Test=SomeValue" to it.
-            Server.Transfer("Target.aspx?Test=SomeValue");//With server side transfrs, you must specify the .aspx extension. This method redirect to a new page on the server side, letting you hide quesry strings from the user.
+            Server.Transfer("Target.aspx?Test=SomeValue");//With server side transfers, you must specify the .aspx extension. This method redirect to a new page on the server side, letting you hide quesry strings from the user.
         }
     }
 }
